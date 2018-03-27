59da8abd71fbc005e26ddcdcdf9b8341af0c6ceb
Rndmkid <tonny.huang0724@gmail.com>
2018-03-27 19:51:43 -0400

    ENT game list added

--- src/Main.py
+++ src/Main.py
@@ -7,6 +7,10 @@ Created on Mar 26, 2018
 from bs4 import BeautifulSoup
 import re 
 import requests
+from selenium import webdriver
+from pandas.io.html import read_html
+
+
 
 def get_Soup(url):
     response = requests.get(url)
@@ -35,13 +39,23 @@ def set_alarm():
     return
 
 def check_ent(gameName, conditionalText):
-    #https://stackoverflow.com/questions/25062365/python-parsing-html-table-generated-by-javascript
+    #Parse https://entgaming.net/forum/games_all_fast.php
     #To-Dp: Use Selenium to scrape the .js table
+    soup = get_Soup("https://entgaming.net/forum/games_all_fast.php")
+    rawText = soup.__getattribute__('text')
+    gamesListRaw = rawText.split("\n")
+    gamesList = []
+    for e in gamesListRaw:
+        try:
+            gamesList.append(e.split("|")[5])
+        except:
+            pass
+        #gamesList.append(e.split("|")[1])
     
     return
 
 def main():
-
+    check_ent("filler", "filler")
     return 
 
 main()
