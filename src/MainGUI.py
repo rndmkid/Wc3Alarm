e5167e52278eb7921e0266451d32b0c1d45a06c9
Rndmkid <tonny.huang0724@gmail.com>
2018-04-04 14:41:32 -0400

    workable state To-Do: save defaults, polish UI, auto wc3, print gn

--- src/MainGUI.py
+++ src/MainGUI.py
@@ -4,18 +4,25 @@ Created on Mar 30, 2018
 @author: Q
 '''
 from appJar import gui
+import BackEnd
+import threading
 
-class MainGUI(object):
+
+
+class GUI(object):
     '''
     classdocs
     '''
     app = gui("Wc3Alarm")
+    gameList = []
+    thread = threading.Thread()
 
     def __init__(self):
         '''
         Constructor
         '''
         self.i = "test"
+        self.gameList = []
         self.app.setBg("white")
         self.app.setFont(18)
         self.app.addLabel("title", "Warcraft 3 Alarm")
@@ -23,23 +30,39 @@ class MainGUI(object):
         self.app.addLabel("search", "Searching For:")
         self.app.stopFrame()
         def press(button):
-            #To-Do Add/Replace Actions
             self.app.openFrame('Games')
             name = self.app.getEntry("Game Name")
             self.app.addLabel(self.i, name)
             self.i = self.i + "t"
             self.app.stopFrame()
+            BackEnd.add_gameNames(self.app.getEntry("key"), self.gameList)
             self.app.setEntry("Game Name", "")
             self.app.setEntry("key", "")
+            self.app.setEntryDefault("Game Name", "Game Name")
+            self.app.setEntryDefault("key", "Keyword, Keyword")
+            if not self.thread:
+                self.thread = threading.Thread(target=BackEnd.thread_actions, args=(self.gameList,))
+                self.thread.start()
+                
+            
         self.app.addEntry("Game Name")
         self.app.addEntry("key")
         self.app.setEntryDefault("Game Name", "Game Name")
         self.app.setEntryDefault("key", "Keyword, Keyword")
         self.app.addButton("Add New Game", press)
-        
+        self.app.setStopFunction(self.thread._stop())
         
     def go(self):
         self.app.go()
+        
     
-    def getKey(self):
-        return self.app.getEntry("key")
+    
+def __getKey(self):
+    return self.app.getEntry("key")
+
+def main():
+    app = GUI()
+    app.go()
+    return
+
+main()
